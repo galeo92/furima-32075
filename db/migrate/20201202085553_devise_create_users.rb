3c1b7870ea30c0ea99352fe092e4156a75dbfbf3
galeo92 <gorimax92@gmail.com>
2020-12-02 18:34:31 +0900

    ユーザーずテーブル作成

--- db/migrate/20201202085553_devise_create_users.rb
+++ db/migrate/20201202085553_devise_create_users.rb
@@ -4,13 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.
-      t.
-      t.
-      t.
-      t.
-
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_frigana, null: false
+      t.string :last_name_frigana,  null: false
+      t.date :birthday,             null: false
+      t.string :email,              null: false, unique: true
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
