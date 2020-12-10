8409304626248b5834268857fcafaf07547ff6f7
galeo92 <gorimax92@gmail.com>
2020-12-10 15:11:48 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
    before_action :authenticate_user!, except:[:index]
 
   def index
-    # @items = Item.all 一覧機能で使う
+    @items = Item.all
   end
 
   def new
